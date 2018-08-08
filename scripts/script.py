3e50f1094323d617ecf87c6a1d05bf70e2a2477a
Shankar Dutt Salwan <Shankar@dhcp-10-105-160-237.wireless.northwestern.private>
2018-08-08 15:33:14 -0500

    Added plot and new datasets

--- scripts/script.py
+++ scripts/script.py
@@ -10,7 +10,9 @@ salary = np.add(dataset["salary"],1)
 years = dataset["time_spend_company"]
 last_eval = dataset["last_evaluation"]
 projects = dataset["number_project"]
+sat_level = dataset["satisfaction_level"]
 
 dataset["ROI"] = np.divide((np.multiply(last_eval,projects)),(np.multiply(salary,years)))
+dataset["attrition_rate"] = np.divide(last_eval,sat_level)
 
-dataset.to_csv("data_with_ROI.csv")
+dataset.to_csv("data_analysis2.csv")
