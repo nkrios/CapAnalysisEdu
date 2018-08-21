2f09c742cffca486862cd807a8d0a9a056838219
Shankar Dutt Salwan <Shankar@dhcp-10-105-160-237.wireless.northwestern.private>
2018-08-21 15:06:40 -0500

    Zoom and legend fixed

--- scripts/script.py
+++ scripts/script.py
@@ -2,19 +2,23 @@ import pandas as pd
 import numpy as np
 from sklearn.preprocessing import LabelEncoder
 
-dataset = pd.read_csv("HR_comma_sep.csv")
+# dataset = pd.read_csv("HR_comma_sep.csv")
 
-dataset["salary"] = LabelEncoder().fit_transform(dataset["salary"])
+# dataset["salary"] = LabelEncoder().fit_transform(dataset["salary"])
 
-salary = np.add(dataset["salary"],1)
-years = dataset["time_spend_company"]
-last_eval = dataset["last_evaluation"]
-projects = dataset["number_project"]
-sat_level = dataset["satisfaction_level"]
+# salary = np.add(dataset["salary"],1)
+# years = dataset["time_spend_company"]
+# last_eval = dataset["last_evaluation"]
+# projects = dataset["number_project"]
+# sat_level = dataset["satisfaction_level"]
 
-dataset["ROI"] = np.divide((np.multiply(last_eval,projects)),(np.multiply(salary,years)))
-dataset["attrition_rate"] = np.divide(last_eval,sat_level)
+# dataset["ROI"] = np.divide((np.multiply(last_eval,projects)),(np.multiply(salary,years)))
+# dataset["attrition_rate"] = np.divide(last_eval,sat_level)
 
-conditions = [((dataset["ROI"]<=149) & (dataset["Risk"]))]
+# conditions = [((dataset["ROI"]>331) & (dataset["Risk"]>1.43))]
 
-dataset.to_csv("data_analysis2.csv")
+
+
+# dataset.to_csv("data_analysis2.csv")
+
+dataset = pd.read_csv(analysisNew.csv)
